4fd44824556fff08c5adeaae8084dc6578e36cde
lynnchen18 <lynnchen821105@gmail.com>
2015-11-23 16:35:09 +0800

    11/23 Initial

---
+++ www/js/app.js
@@ -0,0 +1,142 @@
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.home', {
+    url: '/home',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl'
+      }
+    }
+  })
+
+  .state('app.shopList', {
+    url: '/shopList',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/shopList.html',
+        controller: 'ShopsCtrl'
+      }
+    }
+  })
+
+  .state('app.mealList', {
+    url: '/shop:shopId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/mealList.html',
+        controller: 'HomeCtrl'
+      }
+    }
+  })
+
+  .state('app.myPlate', {
+    url: '/myPlate',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/myPlate.html'
+      }
+    }
+  })
+
+  .state('app.myOrder', {
+    url: '/myOrder',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/myOrder.html'
+      }
+    }
+  })
+
+  .state('app.myFavorite', {
+    url: '/myFavorite',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/myFavorite.html',
+        controller: 'HomeCtrl'
+      }
+    }
+  })
+
+  .state('app.purchaseRecords', {
+    url: '/purchaseRecords',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/purchaseRecords.html'
+      }
+    }
+  })
+
+  .state('app.settings', {
+    url: '/settings',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/settings.html'
+      }
+    }
+  })
+
+  .state('app.about', {
+    url: '/about',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/about.html'
+      }
+    }
+  })
+
+  .state('app.mealInfo', {
+    url: '/meal:productId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/mealInfo.html'
+      }
+    }
+  })
+
+  .state('app.login', {
+    url: '/login',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/login.html'
+      }
+    }
+  })
+
+  .state('app.keeper', {
+    url: '/keeper',
+    views: {
+      'menuContent':{
+        templateUrl: 'templates/keeper.html',
+      }
+    }
+  })
+  ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,174 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $ionicPopup, $timeout) {
+
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+
+  $scope.signup = function() {
+    $scope.data = {};
+
+    // An elaborate, custom popup
+    var signupPopup = $ionicPopup.show({
+      template: 'Name<input type="text" ng-model="data.name"> <br> Phone<input type="tel" ng-model="data.phone"> <br> Email<input type="email" ng-model="data.email"> <br> Password<input type="password" ng-model="data.password">',
+      title: 'Sign up',
+      subTitle: '',
+      scope: $scope,
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: '<b>Sign up</b>',
+          type: 'button-positive',
+          onTap: function(e) {
+            if (!$scope.data.wifi) {
+              //don't allow the user to close unless he enters wifi password
+              e.preventDefault();
+            } else {
+              return $scope.data.wifi;
+            }
+            signupPopup.close();
+            $scope.closeLogin();
+          }
+        }
+      ]
+    });
+    signupPopup.then(function(res) {
+      console.log('Tapped!', res);
+    });
+    $timeout(function() {
+      signupPopup.close(); //close the popup after 3 seconds for some reason
+    }, 10000);
+  };
+})
+
+.controller('HomeCtrl', function($scope) {
+  $scope.products = [
+    {
+      id: "001",
+      name: 'Fruit & Vegs',
+      cover: 'img/fruit-and-veg.jpg',
+      price: 50,
+      like: false
+    },
+    {
+      id: "002",
+      name: 'Pasta',
+      cover: 'img/pasta.jpg',
+      price: 65,
+      like: false
+    },
+    {
+      id: "003",
+      name: '牛肉麵',
+      cover: 'img/beef-noodles.jpg',
+      price: 45,
+      like: false
+    },
+    {
+      id: "004",
+      name: 'Donut',
+      cover: 'img/donuts.jpg',
+      price: 30,
+      like: false
+    },
+    {
+      id: "005",
+      name: 'Cupcake',
+      cover: 'img/cupcakes.jpg',
+      price: 25,
+      like: false
+    },
+  ];
+  $scope.like = function(product) {
+        if(product.like)
+          product.like = false;
+        else
+          product.like = true;
+    };
+})
+
+.controller('ShopsCtrl', function($scope) {
+  $scope.shops = [
+    {
+      id: "01",
+      name: "Shop 1"
+    },
+    {
+      id: "02",
+      name: "Shop 2"
+    },
+    {
+      id: "03",
+      name: "Shop 3"
+    },
+    {
+      id: "04",
+      name: "Shop 4"
+    },
+  ];
+})
+
+
+/*
+.controller('MealCtrl', function($scope) {
+
+
+})
+
+.controller('PlateCtrl', function($scope, $http) {
+ $http.get('https://cors-test.appspot.com/test').then(function(resp) {
+    console.log('Success', resp);
+    // For JSON responses, resp.data contains the result
+  }, function(err) {
+    console.error('ERR', err);
+    // err.status will contain the status code
+  })
+})
+
+.controller('OrderCtrl', function($scope) {
+
+
+})
+
+.controller('FavoriteCtrl', function($scope) {
+
+
+})
+
+.controller('RecordCtrl', function($scope) {
+
+
+})
+
+.controller('LoginCtrl', function($scope) {
+
+
+})
+*/
